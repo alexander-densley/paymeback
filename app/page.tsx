a8f073d9b2ab751e96d775e26d27024fc76d77c9
Alexander Densley <densley.alexander@gmail.com>
2023-10-30 11:49:18 -0600

    update type checking

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,11 @@ import { useState } from 'react'
 export default function Home() {
 	const [names, setNames] = useState<string[]>([])
 	const [name, setName] = useState<string>('')
-	const [amount, setAmount] = useState<number>(0)
-	const [amounts, setAmounts] = useState<number[]>([0])
-
-	const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		e.preventDefault()
-		setName(e.target.value)
-	}
-
-	const handleAddName = () => {
-		if (name === '') return
-		setNames([...names, name])
-		setName('')
-	}
-	const handleRemoveName = (index: number) => {
-		setNames(names.filter((_, i) => i !== index))
-	}
+	const [amount, setAmount] = useState<string>('')
+	const [amounts, setAmounts] = useState<number[]>([])
+	const [items, setItems] = useState<string[]>([])
+	const [item, setItem] = useState<string>('')
+	const [amountError, setAmountError] = useState<boolean>(false)
 
 	return (
 		<div>
@@ -35,9 +24,19 @@ export default function Home() {
 					placeholder='Name'
 					value={name}
 					className='mr-2 text-base'
-					onChange={handleNameChange}
+					onChange={(e) => {
+						setName(e.target.value)
+					}}
 				/>
-				<Button onClick={handleAddName}>Add Name</Button>
+				<Button
+					onClick={() => {
+						if (name === '') return
+						setNames([...names, name])
+						setName('')
+					}}
+				>
+					Add Name
+				</Button>
 			</div>
 			<div className='mt-4 ml-12'>
 				<ul>
@@ -46,12 +45,53 @@ export default function Home() {
 							<Button
 								className='p-3 h-3 mr-2 bg-red-500 text-white'
 								onClick={() => {
-									handleRemoveName(index)
+									setNames(names.filter((_, i) => i !== index))
 								}}
 							>
 								-
 							</Button>
 							<li className='text-lg'>{name}</li>
+						</div>
+					))}
+				</ul>
+			</div>
+
+			{/* stuff */}
+			<div className='flex mt-4 mx-6'>
+				<Input
+					placeholder='Amount'
+					value={amount}
+					className='mr-2 text-base'
+					onChange={(e) => {
+						if (Number.isNaN(Number(e.target.value))) setAmountError(true)
+						else setAmountError(false)
+						setAmount(e.target.value)
+					}}
+				/>
+				<Button
+					onClick={() => {
+						if (amount === undefined || amount === '' || amountError) return
+						setAmounts([...amounts, Number(amount)])
+						setAmount('')
+					}}
+				>
+					Add Amount
+				</Button>
+			</div>
+			{amountError && <p className='text-red-500'>Please enter a number</p>}
+			<div className='mt-4 ml-12'>
+				<ul>
+					{amounts.map((amount, index) => (
+						<div key={index} className='flex'>
+							<Button
+								className='p-3 h-3 mr-2 bg-red-500 text-white'
+								onClick={() => {
+									setAmounts(amounts.filter((_, i) => i !== index))
+								}}
+							>
+								-
+							</Button>
+							<li className='text-lg'>{amount}</li>
 						</div>
 					))}
 				</ul>
